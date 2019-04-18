c7f26675513fedfd8dbd8ab415b67600d8570484
Thareq Yusuf <thareqmyha@gmail.com>
2019-04-18 21:08:40 +0700

    add job model

---
+++ app/src/main/java/com/example/transporterapp/model/Job.java
@@ -0,0 +1,51 @@
+package com.example.transporterapp.model;
+
+import java.util.Date;
+
+public class Job {
+  private int id;
+  private String origin;
+  private String destination;
+  private Date date;
+  private int price;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getOrigin() {
+    return origin;
+  }
+
+  public void setOrigin(String origin) {
+    this.origin = origin;
+  }
+
+  public String getDestination() {
+    return destination;
+  }
+
+  public void setDestination(String destination) {
+    this.destination = destination;
+  }
+
+  public Date getDate() {
+    return date;
+  }
+
+  public void setDate(Date date) {
+    this.date = date;
+  }
+
+  public int getPrice() {
+    return price;
+  }
+
+  public void setPrice(int price) {
+    this.price = price;
+  }
+}
